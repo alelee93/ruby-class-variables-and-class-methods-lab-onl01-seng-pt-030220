174b2ba2a14da7c3326d6ba1adb9061e39a3d477
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-19 03:04:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,16 +29,14 @@ class Song
   end
   
   def self.genre_count
-    genre_count = {}
-    counter = Hash.new(0)
+    #genre_count = {}
+    genre_count = Hash.new(0)
     
-    @@genres.each do |genre|
-    genre_count[genre] = 
+    # @@genres.each do |genre|
+    # genre_count[genre] = 
     
     
-#     @@genres.each do |v|
-#   counter[v] += 1
-# end
+
     
     
     end 
